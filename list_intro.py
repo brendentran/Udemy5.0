11bce6bc2cfe8826f3f1526567edc1c84668de48
brendentran <brenden.tran@bimodalconsulting.com>
2021-08-25 11:22:53 -0700

    Day 1

---
+++ list_intro.py
@@ -0,0 +1,15 @@
+rts = ["computer",
+     "monitor",
+     "keyboard",
+     "mouse",
+     "mouse pad"
+     ]
+
+for part in computer_parts:
+        print(part)
+
+        print ()
+        print(computer_parts[2])
+
+        print(computer_parts[0:3])
+        print(computer_parts[-1])
