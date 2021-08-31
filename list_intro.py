9e24c1e3a9d3f789377efde9a0de558772762bcb
brendentran <brenden.tran@bimodalconsulting.com>
2021-08-30 21:15:13 -0700

    day 4

--- list_intro.py
+++ list_intro.py
@@ -1,15 +1,14 @@
-rts = ["computer",
+computer_parts = ["computer",
      "monitor",
      "keyboard",
      "mouse",
      "mouse pad"
      ]
 
-for part in computer_parts:
-        print(part)
+print(computer_parts)
 
-        print ()
-        print(computer_parts[2])
+# computer_parts[3] = "trackball"
+print(computer_parts)
 
-        print(computer_parts[0:3])
-        print(computer_parts[-1])
+computer_parts[3:] = "trackball"
+print(computer_parts)
