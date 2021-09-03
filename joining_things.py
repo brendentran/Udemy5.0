2c370510162327ee00778290909074dc1317e655
brendentran <brenden.tran@bimodalconsulting.com>
2021-09-02 19:54:25 -0700

    Day 6

---
+++ joining_things.py
@@ -0,0 +1,11 @@
+flowers = [
+        "Daffodil",
+        "Evening Primrose",
+        "Hydrangea",
+        "Iris",
+        "Lavendar",
+        "Sunflower",
+        "Tiger Lily",
+]
+
+print(" | ".join(flowers))
