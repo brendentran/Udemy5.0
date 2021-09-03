123fc047eb6ba708ada3f291d82ab70c2088740b
brendentran <brenden.tran@bimodalconsulting.com>
2021-09-03 08:50:55 -0700

    Day 7

---
+++ unpacking.py
@@ -0,0 +1,21 @@
+a = b = c = d = e = f = 42
+print(c)
+
+x, y = 1, 2
+print(x)
+print(y)
+
+print("Unpacking a tuple")
+data = 1, 2, 37
+print(x)
+print(y)
+print(z)
+
+print("Unpacking a list")
+
+data_list = [12, 13, 14]
+p, q, r = data_list
+print(p)
+print(q)
+print(r)
+
